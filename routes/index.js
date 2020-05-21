d8df2d4fc1176a0bf6a460c8d875b67de4966d56
Thibault-d <thibaultdelfaud@gmail.com>
2020-05-21 19:21:54 +0200

    solved issue with Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const Stroller = require("../models/Stroller.js");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  Stroller.find()
-  .then((strollers) => {
-    res.status(200).json(strollers);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
